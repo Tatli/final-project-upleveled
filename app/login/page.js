3afccbac2ba80f3c36dbe88437722b5e3dca186a
Osman Tatli <tatli.osman@gmx.at>
2023-11-01 19:06:41 +0100

    Remove duplicate routes

--- app/login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from '../components/login/daisyui/LoginForm';
-
-export default function Login() {
-  return (
-    <div className="flex">
-      <LoginForm />
-    </div>
-  );
-}
--- app/register/page.js
+++
@@ -1,9 +0,0 @@
-import RegisterForm from '../components/register/daisyui/RegisterForm';
-
-export default function Register() {
-  return (
-    <div className="flex justify-center items-center">
-      <RegisterForm />
-    </div>
-  );
-}
