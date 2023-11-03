c4c598c8105e215c6a3a5a36b8c58e9fb8f79977
Osman Tatli <tatli.osman@gmx.at>
2023-11-03 15:07:50 +0100

    Add route and frontend for listing creation

---
+++ app/newlisting/Categories.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <>
+      <button
+        className="btn w-1/2"
+        onClick={() => document.getElementById('my_modal_1').showModal()}
+      >
+        Show Categories
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
