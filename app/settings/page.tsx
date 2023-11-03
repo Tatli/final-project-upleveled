6b91a4e1fb15f5f0bd7a35bc7c6171b5712ebb5b
Osman Tatli <tatli.osman@gmx.at>
2023-11-03 17:47:24 +0100

    Change settings UI

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 import { getClient } from '../../util/apolloClient';
 
 export default async function Settings() {
+  // const [profileType, setProfileType] = useState('private');
   const fakeSessionToken = cookies().get('fakeSession');
 
   const { data } = await getClient().query({
@@ -26,21 +27,21 @@ export default async function Settings() {
   }
 
   return (
-    <div className={`grid grid-cols-12 pt-16`}>
-      <div className={`col-span-2`}>
-        <h1 className={`text-5xl pb-4`}>Settings</h1>
+    <div className="grid grid-cols-12 pt-16">
+      <div className="col-span-1">
+        <h1 className="text-3xl pb-4">Settings</h1>
         <hr />
         <br />
         <ul>
-          <li className={`text-primary`}>Profile</li>
+          <li className="text-primary">Profile</li>
         </ul>
       </div>
-      <div className={`col-span-1`}></div>
-      <div className={`col-span-5`}>
-        <h1 className={`text-5xl pb-4`}>Profile</h1>
+      <div className="col-span-1" />
+      <div className="col-span-5">
+        <h1 className="text-3xl pb-4">Profile</h1>
         <hr />
         <br />
-        <h2 className={`text-3xl pb-2`}>Personal Information</h2>
+        <h2 className="text-2xl pb-2">Personal Information</h2>
         <div className="form-control w-full">
           <label className="label">
             <span className="label-text">First name</span>
@@ -67,7 +68,7 @@ export default async function Settings() {
             className="input input-bordered w-full mb-2"
           />
           {/* ### Address ### */}
-          <h2 className={`text-3xl my-2`}>Address</h2>
+          <h2 className="text-2xl my-2">Address</h2>
           <label className="label">
             <span className="label-text">Street</span>
           </label>
@@ -100,11 +101,11 @@ export default async function Settings() {
             placeholder="Postal code"
             className="input input-bordered w-full mb-2"
           />
-          <h2 className={`text-3xl my-2`}>Private information</h2>
+          <h2 className="text-2xl my-2">Private information</h2>
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <div className={`join`}>
+          <div className="join">
             <input
               type="email"
               placeholder="your@email.xyz"
@@ -116,7 +117,7 @@ export default async function Settings() {
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <div className={`join`}>
+          <div className="join">
             <input
               type="password"
               placeholder="password"
@@ -139,44 +140,47 @@ export default async function Settings() {
 
       <div className={`col-span-1`}></div>
 
-      <div className={`col-span-2 mt-24`}>
-        <div className={`flex flex-col`}>
-          <h2 className={`text-3xl mb-8`}>Profile picture</h2>
+      <div className="col-span-4">
+        <div className="flex flex-col">
+          <h2 className="text-2xl mb-8">Profile picture</h2>
           <Image
             src="/images/profile/default-male.jpg"
             width={285}
             height={516}
             alt="Picture of the user"
-          ></Image>
+          />
           <input
             type="file"
-            className="file-input file-input-bordered file-input-primary w-full mt-2"
+            className="file-input file-input-bordered file-input-primary w-4/6 mt-2"
           />
-          <h2 className={`text-3xl my-4`}>Profile type</h2>
+          <h2 className="text-2xl my-4">Profile type</h2>
+
+          {/* Move this out into a client component for state management */}
           <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Private</span>
-              <input
-                type="radio"
-                name="profileType"
-                className="radio checked:bg-primary"
-                checked
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Commercial</span>
-              <input
-                type="radio"
-                name="profileType"
-                className="radio checked:bg-primary"
-              />
-            </label>
+            <div className="w-2/3">
+              <label className="label cursor-pointer">
+                <span className="label-text">Private</span>
+                <input
+                  type="radio"
+                  name="profileType"
+                  className="radio checked:bg-primary"
+                />
+              </label>
+            </div>
+            <div className="w-2/3">
+              <label className="label cursor-pointer">
+                <span className="label-text">Commercial</span>
+                <input
+                  type="radio"
+                  name="profileType"
+                  className="radio checked:bg-primary"
+                />
+              </label>
+            </div>
           </div>
         </div>
       </div>
-      <div className={`col-span-1`}></div>
+      <div className="col-span-1"></div>
     </div>
   );
 }
