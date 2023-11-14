09d09189820ea3d3bed37eddf62d86d2a473ca25
Osman Tatli <tatli.osman@gmx.at>
2023-11-14 16:44:32 +0100

    Add redirect to home after logout

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import { logout } from './action';
 
 export function LogoutButton() {
@@ -10,6 +10,7 @@ export function LogoutButton() {
       <button
         formAction={async () => {
           await logout();
+          redirect('/');
           router.refresh();
         }}
       >
